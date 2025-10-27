a31d751477a6addafa0306ee45e8c50e71f1a6c6
Júlia Manuela Turnes <jmturnes10@gmail.com>
2025-10-27 14:16:53 -0300

    feat: criando e configurando a área de playlist da side bar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,49 +1,77 @@
-import { useState } from "react";
+import { useState } from "react"
 import style from "./style.module.css"
-import { IoIosArrowForward, IoIosArrowBack   } from "react-icons/io";
+import { IoIosArrowForward, IoIosArrowBack, IoIosHelpCircle } from "react-icons/io";
 import { MdOutlineLibraryAdd, MdPlaylistPlay, MdAccountCircle } from "react-icons/md";
 
-const Sidebar = () =>{
-    const [isOpen, setIsOpen] = useState(false);
+const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [isPlaylistOpen, setIsPlaylistOpen] = useState(false)
 
-    return(
-        <div className={`${style.sidebar} ${isOpen? style.open : " "}`}>
+    const playlists = ["fav da sofia 2.0", "melhores da loirah", "fav da sofia", "pipi pôpô pipi pôpô"]
 
-            <p>Logotipo</p>
+    return (
+        <div className={`${style.sidebar} ${isOpen ? style.open : " "}`}>
+            <div className={style.logo}>
+                <div className={style.soundwave}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                {isOpen && <p>Moodify</p>}
+            </div>
 
             <nav>
-                <ul className={style.icons}>
-
-                    <li onClick={()=>{setIsOpen(!isOpen)}}>
-                        <span> {isOpen? <IoIosArrowForward/> : <IoIosArrowBack /> }</span>
-                        <p>{isOpen? "Fechar" : ""}</p>
+                <ul className={`${style.icons} ${style.position}`}>
+                    <li
+                        onClick={() => {
+                            setIsOpen(!isOpen)
+                        }}
+                    >
+                        <span> {isOpen ? <IoIosArrowForward /> : <IoIosArrowBack />}</span>
+                        <p>{isOpen ? "Fechar" : ""}</p>
                     </li>
 
                     <li>
-                        <MdOutlineLibraryAdd/>
-                        <p> {isOpen? "Nova Playlist" : ""} </p>
+                        <MdOutlineLibraryAdd />
+                        <p> {isOpen ? "Nova Playlist" : ""} </p>
                     </li>
 
-                    
-                    <li>
-                        <MdPlaylistPlay/>
-                        <p> {isOpen? "Playlists" : ""} </p>
+                    <li
+                        onClick={() => {
+                            setIsPlaylistOpen(!isPlaylistOpen)
+                        }}
+                    >
+                        <MdPlaylistPlay />
+                        <p> {isOpen ? "Playlists" : ""} </p>
                     </li>
 
-                    <p> {isOpen? "<lista>" : "" }</p>
-                    
+                    {isOpen && isPlaylistOpen && (
+                        <li >
+                            <div className={style.playlists}>
+                                {playlists.map((playlist, index) => (
+                                    <li key={index}>
+                                        <p>{playlist}</p>
+                                    </li>
+                                ))}
+                            </div>
+                        </li>
+                    )}
 
                     <li className={style.account_icon}>
                         <MdAccountCircle></MdAccountCircle>
-                        <p>{isOpen? "Perfil" : "" }</p>
+                        <p>{isOpen ? "Perfil" : ""}</p>
                     </li>
 
+                    <li className={style.help_icon}>
+                        <IoIosHelpCircle ></IoIosHelpCircle >
+                        <p>{isOpen ? "Ajuda" : ""}</p>
+                    </li>
                 </ul>
             </nav>
-
         </div>
     )
-
 }
 
 export default Sidebar
