850b09d5231b805991b6d923407bc6a428ed020c
thays <thaysdasilvamariano@gmail.com>
2025-12-20 12:02:30 -0300

    Alterada mensagem da página inicial para melhor explicação do sistema

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -5,7 +5,7 @@ const Title = () =>{
     return <>
     <div className={style.container}>
         <h1>Qual seu <em>mood</em> hoje? </h1>
-        <p>Manda seu <em>mood</em> que a gente entrega a vibe perfeita.</p>
+        <p>Mande seu <em>mood</em> e receba playlists únicas, criadas na hora e personalizadas de acordo com o que você está sentindo</p>
     </div> 
     </>
 
